bc3a9f21262ee297d97ff25637e28f5c20ca7166
Pritom Mazumder <pritommazumder@gmail.com>
2025-04-22 13:10:00 +0200

    [CHORE] Initialize Django project with base configuration (developments.py)

---
+++ config/settings/development.py
@@ -0,0 +1,15 @@
+"""Development settings."""
+
+from .base import *  # noqa: F401, F403
+
+DEBUG = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# Use console email backend
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Debug toolbar
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+INTERNAL_IPS = ["127.0.0.1"]
