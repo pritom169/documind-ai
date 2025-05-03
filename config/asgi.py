dbd04f8a78c4e2282e2f0a767722ee2cdd552f48
Pritom Mazumder <pritommazumder@gmail.com>
2025-05-03 10:15:00 +0200

    [Feature] add ASGI configuration with WebSocket support (asgi.py)

---
+++ config/asgi.py
@@ -0,0 +1,23 @@
+"""ASGI configuration with Django Channels for WebSocket support."""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+django_asgi_app = get_asgi_application()
+
+from apps.conversations.routing import websocket_urlpatterns  # noqa: E402
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
